93692e8b71e1809c833bddda710ef4a93ededbcf
Asher Samuel <ashersam116@gmail.com>
2026-01-23 11:17:57 +0300

    00018-easy-tuple-length

--- challenges/playground.ts
+++ challenges/playground.ts
@@ -1,4 +1,13 @@
-const arr = [1, 2, 3, 4];
-const [p, ...rest] = arr;
+import type { Length } from "./00018-easy-tuple-length";
 
-console.log(p);
+type tesla = ["tesla", "model 3", "model X", "model Y"];
+type spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT",
+];
+
+type teslaLength = Length<tesla>; // expected 4
+type spaceXLength = Length<spaceX>; // expected 5
