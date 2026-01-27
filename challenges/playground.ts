f58f61606eb372d75fb22b95bfd9a7c49897e4af
Asher Samuel <ashersam116@gmail.com>
2026-01-27 09:25:23 +0300

    03312-easy-parameters

--- challenges/playground.ts
+++ challenges/playground.ts
@@ -1,3 +1,5 @@
-import type { Unshift } from "./03060-easy-unshift";
+import type { MyParameters } from "./03312-easy-parameters";
 
-type Result = Unshift<[1, 2], 0>; // [0, 1, 2]
+const foo = (arg1: string, arg2: number): void => {};
+
+type FunctionParamsType = MyParameters<typeof foo>; // [arg1: string, arg2: number]
