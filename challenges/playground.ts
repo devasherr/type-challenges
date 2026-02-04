eec49cb8e5f5173335884223f102c64b1afb3a6e
Asher Samuel <ashersam116@gmail.com>
2026-02-04 12:09:47 +0300

    00012-medium-chainable-options

--- challenges/playground.ts
+++ challenges/playground.ts
@@ -1,5 +1,20 @@
-import type { TupleToUnion } from "./00010-medium-tuple-to-union";
+import type { Chainable } from "./00012-medium-chainable-options";
 
-type Arr = ["1", "2", "3"];
+declare const config: Chainable;
 
-type Test = TupleToUnion<Arr>; // expected to be '1' | '2' | '3'
+const result = config
+  .option("foo", 123)
+  .option("name", "type-challenges")
+  .option("bar", { value: "Hello World" })
+  .get();
+
+// expect the type of result to be:
+interface Result {
+  foo: number;
+  name: string;
+  bar: {
+    value: string;
+  };
+}
+
+const result3 = config.option("name", "another name").option("name", 123).get();
