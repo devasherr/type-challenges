cca5fa05ba916d70518240494c6e2e180d10de45
Asher Samuel <ashersam116@gmail.com>
2026-02-14 23:08:15 +0300

    01042-medium-isnever

--- challenges/playground.ts
+++ challenges/playground.ts
@@ -1,4 +1,7 @@
-import type { AnyOf } from "./00949-medium-anyof";
+import type { IsNever } from "./01042-medium-isnever";
 
-type Sample1 = AnyOf<[1, "", false, [], {}]>; // expected to be true.
-type Sample2 = AnyOf<[0, "", false, [], {}]>; // expected to be false
+type A = IsNever<never>; // expected to be true
+type B = IsNever<undefined>; // expected to be false
+type C = IsNever<null>; // expected to be false
+type D = IsNever<[]>; // expected to be false
+type E = IsNever<number>; // expected to be false
