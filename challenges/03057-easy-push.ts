0933f4f9f0c27163e673c4c8deecfcdb2e9d45ae
Asher Samuel <ashersam116@gmail.com>
2026-05-13 22:15:26 +0300

    03057-easy-push *again

--- challenges/03057-easy-push.ts
+++ challenges/03057-easy-push.ts
@@ -18,23 +18,23 @@
 
 /* _____________ Your Code Here _____________ */
 
-export type Push<T extends unknown[], U> = [...T, U];
+export type Push<T extends any[], U> = [...T, U]
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from "../utils/";
+import type { Equal, Expect } from '../utils/'
 
 type cases = [
   Expect<Equal<Push<[], 1>, [1]>>,
-  Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
-  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>,
-];
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
 
 type errors = [
   // @ts-expect-error
   Expect<Equal<Push<number[], string>, string[]>>,
   // @ts-expect-error
   Expect<Equal<Push<string[], number>, [string, number]>>,
-];
+]
 
 /* _____________ Further Steps _____________ */
 /*
@@ -42,3 +42,4 @@ type errors = [
   > View solutions: https://tsch.js.org/3057/solutions
   > More Challenges: https://tsch.js.org
 */
+
