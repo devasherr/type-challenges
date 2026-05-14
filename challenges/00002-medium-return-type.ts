2d6724570f36144bd1ac5b06c8adafbc65454cdc
Asher Samuel <ashersam116@gmail.com>
2026-05-14 14:34:36 +0300

    00002-medium-return-type *again

--- challenges/00002-medium-return-type.ts
+++ challenges/00002-medium-return-type.ts
@@ -25,33 +25,29 @@
 
 /* _____________ Your Code Here _____________ */
 
-export type MyReturnType<T extends (...args: any[]) => any> = T extends (
-  ...args: any[]
-) => infer R
-  ? R
-  : never;
+export type MyReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer R ? R : never
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from "../utils/";
+import type { Equal, Expect } from '../utils/'
 
 type cases = [
   Expect<Equal<string, MyReturnType<() => string>>>,
   Expect<Equal<123, MyReturnType<() => 123>>>,
   Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
   Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
-  Expect<Equal<() => "foo", MyReturnType<() => () => "foo">>>,
+  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
   Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
   Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
-];
+]
 
 type ComplexObject = {
-  a: [12, "foo"];
-  bar: "hello";
-  prev(): number;
-};
+  a: [12, 'foo']
+  bar: 'hello'
+  prev(): number
+}
 
-const fn = (v: boolean) => (v ? 1 : 2);
-const fn1 = (v: boolean, w: any) => (v ? 1 : 2);
+const fn = (v: boolean) => v ? 1 : 2
+const fn1 = (v: boolean, w: any) => v ? 1 : 2
 
 /* _____________ Further Steps _____________ */
 /*
@@ -59,3 +55,4 @@ const fn1 = (v: boolean, w: any) => (v ? 1 : 2);
   > View solutions: https://tsch.js.org/2/solutions
   > More Challenges: https://tsch.js.org
 */
+
